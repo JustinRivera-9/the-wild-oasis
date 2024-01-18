a2f151589a948bcc69fe91cf0f8903271e911095
JustinRivera-9 <justin_rivera9@outlook.com>
2024-01-17 18:01:18 -0700

    updated

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,22 +1,19 @@
-import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  const [cabin, setCabin] = useState();
-  useEffect(() => {
-    getCabins().then((data) => setCabin(data[0]));
-  }, []);
-
-  if (!cabin) return <h1>Loading</h1>;
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src={cabin.image} alt="cabin" />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
